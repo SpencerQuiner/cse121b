a24ad291d557b2deccac14abef6f58672c71fb15
SpencerQuiner <spencerquiner@gmail.com>
2023-11-13 13:01:11 +0000

    Week 3 operator and comparison activity.

---
+++ week2/Operators.js
@@ -0,0 +1,13 @@
+x = 5;
+y = 10;
+
+addition = x + y;
+multiplication = x * y;
+subtract = y - x;
+concat = 'bob\'s' + 'your uncle';
+assign = 'a value';
+incrementByThree = x+3;
+x == Y;
+x < y;
+x != y;
+y < 10 && y >0;
