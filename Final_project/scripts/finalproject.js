0e42d5b5102a1d8aa72a69b43cfd5ea80a5677b2
SpencerQuiner <spencerquiner@gmail.com>
2023-12-05 12:06:25 +0000

    update to JSON file and fetch function

--- Final_project/scripts/finalproject.js
+++ Final_project/scripts/finalproject.js
@@ -1,3 +1,13 @@
-/*async function load data from JSON file*/
 
+let memberList = [];
+
+
+/*async function load data from JSON file*/
+const getMembers = async () => {
+    const response = await fetch('https://github.com/SpencerQuiner/cse121b/blob/main/Final_project/resources/members.json', {mode: "no-cors"});
+    memberList = await response.json();
+    console.log(memberList);
+}
+
+getMembers();
 /*eventlistener*/
