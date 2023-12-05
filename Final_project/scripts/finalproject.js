aa2df66bc27a4e3c6b06bdb6050acae3ea4266ee
Spencer <47122213+SpencerQuiner@users.noreply.github.com>
2023-12-05 12:36:54 +0000

    update fetch url

--- Final_project/scripts/finalproject.js
+++ Final_project/scripts/finalproject.js
@@ -4,7 +4,7 @@ let memberList = [];
 
 /*async function load data from JSON file*/
 const getMembers = async () => {
-    const response = await fetch('https://github.com/SpencerQuiner/cse121b/blob/main/Final_project/resources/members.json', {mode: "no-cors"});
+    const response = await fetch('/Final_project/resources/members.json', {mode: "no-cors"});
     memberList = await response.json();
     console.log(memberList);
 }
