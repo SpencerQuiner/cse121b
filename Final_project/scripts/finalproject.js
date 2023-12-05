de0d0f396b43ed57d54187d508b848c2d350d50c
SpencerQuiner <spencerquiner@gmail.com>
2023-12-05 14:33:40 +0000

    updates to finalproject.js

--- Final_project/scripts/finalproject.js
+++ Final_project/scripts/finalproject.js
@@ -12,20 +12,15 @@ function CheckCredentials(members)
 
 /*async function load data from JSON file*/
 const getMembers = async () => {
-<<<<<<< HEAD
-    const response = await fetch('http://127.0.0.1:5500/Final_project/resources/members.json', {mode: "no-cors"});
-=======
-    const response = await fetch('/Final_project/resources/members.json', {mode: "no-cors"});
->>>>>>> aa2df66bc27a4e3c6b06bdb6050acae3ea4266ee
+    
     const response = await fetch('/Final_project/resources/members.json', {mode: "no-cors"});
     memberList = await response.json();
     console.log(memberList);
 }
 
 getMembers();
-<<<<<<< HEAD
+
 
 /*eventlistener*/
 document.querySelector('#loginBtn').addEventListener('click',CheckCredentials);
-=======
-/*eventlistener*/
+
